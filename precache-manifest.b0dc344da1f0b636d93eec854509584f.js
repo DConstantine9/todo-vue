8562b8d26bba2b80301b207c1f913e35f7fc4e8b
DConstantine9 <constantinedeniskkin@gmail.com>
2020-09-07 11:26:05 +0300

    Initial dist subtree commit

---
+++ precache-manifest.b0dc344da1f0b636d93eec854509584f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d40b9951adc029d18e5b",
+    "url": "/todo-vue/css/app.dce10b8e.css"
+  },
+  {
+    "revision": "48748d21f70987fa2001c4fe0049f822",
+    "url": "/todo-vue/index.html"
+  },
+  {
+    "revision": "d40b9951adc029d18e5b",
+    "url": "/todo-vue/js/app.e168aa8c.js"
+  },
+  {
+    "revision": "6c150b96646d1c2eb02d",
+    "url": "/todo-vue/js/chunk-vendors.be8bfcdd.js"
+  },
+  {
+    "revision": "9ba57f9b60e6bd57955b45c89fcd7b66",
+    "url": "/todo-vue/manifest.json"
+  }
+]);
